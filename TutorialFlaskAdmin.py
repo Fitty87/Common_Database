65a4de478a280a0b1a3fa353f16dd1ea23670096
Sascha Wolf <sascha-wo@gmx.de>
2022-05-02 22:33:40 +0200

    StandBevorFlaskAdminStart

--- TutorialFlaskAdmin.py
+++ TutorialFlaskAdmin.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_admin import Admin
 #from flask_admin.contrib.sqla import ModelView
 
@@ -8,8 +11,14 @@ app = Flask(__name__)
 
 #Add Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///oe24Example.db'
+
+#Secret Key
+app.config['SECRET_KEY'] = "my secret key" 
+
 #Initialize the database
 db = SQLAlchemy(app)
+
+
 
 #Create Model
 class Datenquelle(db.Model):
@@ -21,6 +30,21 @@ class Datenquelle(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
+class Kunde(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    geburtsdatum = db.Column(db.DateTime, nullable = False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+#Create InsertForms-----
+class FormDatenquelle(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class FormDatenquelle(FlaskForm):
+name = StringField("Name", validators=[DataRequired()])
+submit = SubmitField("Submit")
 
 # set optional bootswatch theme
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -32,13 +56,35 @@ admin = Admin(app, name='microblog', template_mode='bootstrap3')
 def index():
     return render_template('index.html')
 
-@app.route('/Add_Datenquelle')
-def add_Datenquelle():
-    return render_template('add_datenquelle.html')
+@app.route('/Database')
+def database():
+    allDatenquelle = Datenquelle.query.order_by(Datenquelle.date_added)
+    return render_template('database.html', allDatenquelle=allDatenquelle)
 
-@app.route('/Add_Kunde')
+@app.route('/Add_Datenquelle', methods=['GET', 'POST'])
+def add_Datenquelle():
+    name = None
+    form = FormDatenquelle()
+
+    if form.validate_on_submit():
+        datenquelle = Datenquelle.query.filter_by(name=form.name.data).first()
+        if datenquelle is None:
+            datenquelle = Datenquelle(name=form.name.data)
+            db.session.add(datenquelle)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+         
+    allDatenquelle = Datenquelle.query.order_by(Datenquelle.date_added)
+
+    return render_template('add_datenquelle.html', form=form, name=name, allDatenquelle=allDatenquelle)
+
+@app.route('/Add_Kunde', methods=['GET', 'POST'])
 def add_Kunde():
-    return render_template('add_kunde.html')
+    allDatenquelle = Datenquelle.query.order_by(Datenquelle.date_added)
+    name = None
+    form = FormDatenquelle()
+    return render_template('add_kunde.html', form=form, name=name, allDatenquelle=allDatenquelle)
 
 @app.route('/Add_Rechnung')
 def add_Rechnung():
