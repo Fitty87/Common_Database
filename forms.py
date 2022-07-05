8d88f760afc4b181df5e5db0a613eddb2dacb451
Sascha Wolf <sascha-wo@gmx.de>
2022-07-05 11:10:25 +0200

    user_login_form2

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
+
+class LoginForm(FlaskForm):
+    #@Tom: Hier auf Lenght auch prüfen gut?
+    email = StringField('email', validators=[InputRequired(), Length(min=10, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=20)])
